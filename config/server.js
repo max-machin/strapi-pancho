aa605481ce43eeffe6fb0c901fe9af924db752eb
Machin Max <machin.max@laplateforme.io>
2023-12-01 21:38:25 +0100

    push admin conf

--- config/server.js
+++ config/server.js
@@ -7,4 +7,10 @@ module.exports = ({ env }) => ({
   webhooks: {
     populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
+  admin: {
+    path: '/admin',
+    build: {
+      backend: env('ADMIN_BUILD_BACKEND', 'https://pancho-gyps-api.fr'),
+    },
+  },
 });
